a621072155fe2fd4461aa6ee2fbb3a2d0ea74587
pal-NIKHIL <94384228+pal-NIKHIL@users.noreply.github.com>
2023-05-23 11:55:46 +0530

    initial

---
+++ server/controllers/posts.js
@@ -0,0 +1,69 @@
+import Post from "../models/Post.js"
+import User from "../models/user.js";
+export const createPost = async (req,res)=>{
+    try {
+        const {userId,description,picturePath}=req.body
+        const user= await User.findById(userId)
+        const newPost =new Post({
+            userId,
+            firstName:user.firstName,
+            lastName:user.lastName,
+            location:user.location,
+            description,
+            picturePath,
+            userPicturePath:user.picturePath,
+            likes:{},
+            comments:[]
+        })
+        await newPost.save()
+        const post = await Post.find()
+        res.status(201).json(post)
+    }
+    catch (e) {
+        res.status(409).json({message:e.message})
+    }
+}
+/*Read */
+export const getFeedPosts = async (req,res)=>{
+    try{
+        const post = await Post.find()
+        res.status(201).json(post)
+    }
+    catch (e) {
+        res.status(404).json({message:e.message})
+    }
+}
+
+export const getUserPosts = async (req,res)=>{
+    try{
+        const {userId}=req.params
+        const post = await Post.find({userId})
+        res.status(200).json(post)
+
+    }
+    catch (e) {
+        res.status(404).json({message:e.message})
+    }
+}
+
+/*update*/
+
+export const likePost =async (req,res)=>{
+    try{
+        const {id}=req.params
+        const {userId}=req.body
+        const post=await Post.findById(id)
+        const isLiked=post.likes.get(userId)
+        if(isLiked){
+            post.likes.delete(userId)
+        }
+        else{
+            post.likes.set(userId,true)
+        }
+        const updatedPost =await Post.findByIdAndUpdate(id,{likes:post.likes},{new:true})
+        res.status(200).json(updatedPost)
+    }
+    catch (e) {
+        res.status(404).json({message:e.message})
+    }
+}
